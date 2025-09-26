6b99a16c41e57389476eacb1218225f33ea126cb
MD. Mahafujur Rahman Tuhin <mrtuhin444@gmail.com>
2025-09-26 18:42:41 +0600

    problem 2

--- bin/data_type_problem/problem_02.dart
+++ bin/data_type_problem/problem_02.dart
@@ -7,7 +7,10 @@ second item - double
 void main() {
   int firstItemPrice = 100;
   double secondItemPrice = 33.55;
+  double totalPrice = firstItemPrice+secondItemPrice;
+ print('First Price is = $firstItemPrice');
+ print('Second Price is = $secondItemPrice');
 
-
+  print('Total Price is : $totalPrice');
 
 }
