12d7bdc5e060cfc35c4710cb785bed57d35761df
MD. Mahafujur Rahman Tuhin <mrtuhin444@gmail.com>
2025-09-26 17:48:31 +0600

    problem 02

---
+++ bin/data_type_problem/problem_02.dart
@@ -0,0 +1,13 @@
+// dart problem 02
+/*
+first and second item price problem solution this problem
+first item - int
+second item - double
+ */
+void main() {
+  int firstItemPrice = 100;
+  double secondItemPrice = 33.55;
+
+
+
+}
