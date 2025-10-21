5a6e50067f196ca3623d15b3fa48017be793d1ca
MD. Mahafujur Rahman Tuhin <mrtuhin444@gmail.com>
2025-10-21 15:14:04 +0600

    dart arithmetic operator problem solution

---
+++ bin/dart_operator/arithmetic _operator/problem_01.dart
@@ -0,0 +1,13 @@
+// dart problem
+// problem = print(15 - (4 + 3));
+void main() {
+  int a = 15, b = 4 , c= 3;
+  int calculation  = (a -(b+c));
+  print('Final Result = $calculation');
+
+  //print(9 * (2 + 1));
+  int m = 9, n =2, o= 1;
+  int totalNumber = (m * (n +o));
+  print('Total Number = $totalNumber');
+
+}
